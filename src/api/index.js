4fc9da1eeede0593f3fe59609be0c5ee50cb50ff
turki <trkyable@gmail.com>
2023-09-02 17:26:37 +0300

    working

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
-  // baseURL: "https://keewee-be-652769a3d7b1.herokuapp.com",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://keewee-be-652769a3d7b1.herokuapp.com",
 });
 
 export default instance;
