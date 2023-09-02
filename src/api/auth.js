e40d33411b492bfc7cd56a6c5a51d889a4cbcd37
turki <trkyable@gmail.com>
2023-09-02 17:15:43 +0300

    working

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,7 @@
 import instance from ".";
 
 export const resetpassword = async (token, data) => {
+  console.log(token, data);
   const res = await instance.patch(`/api/auth/resetPassword/${token}`, data);
   return res.data;
 };
--- src/pages/ResetPass.js
+++ src/pages/ResetPass.js
@@ -1,29 +1,149 @@
-import { useMutation } from "@tanstack/react-query";
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 import { resetpassword } from "../api/auth";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import "font-awesome/css/font-awesome.min.css";
+
 export const ResetPass = () => {
   const { token } = useParams();
   const [data, setData] = useState("");
-  const { mutate } = useMutation(["resetpassword", token], () => {
-    // api call
-    return resetpassword(token, data);
+  const [success, setSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const { mutate, isSuccess, isError } = useMutation(
+    ["resetpassword", token],
+    () => {
+      return resetpassword(token, data);
+    },
+    {
+      onSuccess: () => setSuccess(true),
+      onError: () => setSuccess(false),
+    }
+  );
+
+  const initialValues = {
+    password: "",
+    confirmPassword: "",
+  };
+
+  const validationSchema = Yup.object({
+    password: Yup.string().required("Password is required"),
+    confirmPassword: Yup.string()
+      .required("Confirm password is required")
+      .oneOf([Yup.ref("password"), null], "Passwords must match"),
   });
 
+  const handleFormSubmit = (values) => {
+    setData({ password: values.password });
+    mutate();
+  };
+
   return (
-    <div>
-      <h1>Reset Password</h1>
-      <input
-        type="password"
-        placeholder="New Password"
-        onChange={(e) => setData({ ...data, password: e.target.value })}
-      />
-      <input
-        type="password"
-        placeholder="Confirm Password"
-        onChange={(e) => setData({ ...data, confirmPassword: e.target.value })}
-      />
-      <button onClick={mutate}>Reset Password</button>
+    <div
+      style={{ height: "70vh" }}
+      className=" w-full flex items-center justify-center p-7"
+    >
+      <div
+        className={`max-w-sm w-full px-4 py-4 bg-white rounded-md shadow-md border border-gray-600 flex flex-col items-center justify-center ${
+          success ? "animate-bounce" : ""
+        }`}
+      >
+        <h1 className="text-2xl text-black font-semibold mb-4 text-center">
+          Reset Password
+        </h1>{" "}
+        {isError && (
+          <div className="mb-4 w-full text-center text-white bg-red-500 p-2 rounded">
+            Error resetting password.
+          </div>
+        )}
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleFormSubmit}
+        >
+          {({ errors, touched }) => (
+            <Form className="w-full flex flex-col items-center">
+              <div className="mb-2 flex flex-col items-center relative w-full">
+                <label
+                  htmlFor="password"
+                  className="text-black text-xs font-medium mb-1 text-center"
+                >
+                  New Password
+                </label>
+                <div className="relative">
+                  <Field
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    className={`px-2 py-1 text-sm border rounded-md w-full ${
+                      errors.password && touched.password
+                        ? "border-red-500"
+                        : "border-gray-300"
+                    }`}
+                  />
+                  <div className="absolute inset-y-0 right-0 pr-2 flex items-center text-sm leading-5">
+                    <i
+                      onClick={() => setShowPassword(!showPassword)}
+                      className={`fa ${
+                        showPassword ? "fa-eye-slash" : "fa-eye"
+                      } cursor-pointer`}
+                    ></i>
+                  </div>
+                </div>
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-red-500 text-xs font-medium text-center"
+                />
+              </div>
+              <div className="mb-2 flex flex-col items-center relative w-full">
+                <label
+                  htmlFor="confirmPassword"
+                  className="text-black text-xs font-medium mb-1 text-center"
+                >
+                  Confirm Password
+                </label>
+                <div className="relative">
+                  <Field
+                    name="confirmPassword"
+                    type={showConfirmPassword ? "text" : "password"}
+                    className={`px-2 py-1 text-sm border rounded-md w-full ${
+                      errors.confirmPassword && touched.confirmPassword
+                        ? "border-red-500"
+                        : "border-gray-300"
+                    }`}
+                  />
+                  <div className="absolute inset-y-0 right-0 pr-2 flex items-center text-sm leading-5">
+                    <i
+                      onClick={() =>
+                        setShowConfirmPassword(!showConfirmPassword)
+                      }
+                      className={`fa ${
+                        showConfirmPassword ? "fa-eye-slash" : "fa-eye"
+                      } cursor-pointer`}
+                    ></i>
+                  </div>
+                </div>
+                <ErrorMessage
+                  name="confirmPassword"
+                  component="div"
+                  className="text-red-500 text-xs font-medium text-center"
+                />
+              </div>
+              <div className="flex justify-center w-full">
+                <button
+                  type="submit"
+                  className="px-2 py-1 bg-blue-500 text-white rounded-md text-sm hover:bg-blue-600"
+                >
+                  Reset Password
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
