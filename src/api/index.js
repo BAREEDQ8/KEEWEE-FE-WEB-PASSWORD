7012c2cc2a0396fc20d6b0b700eaf28fc1a9130f
turki <trkyable@gmail.com>
2023-11-12 21:17:59 +0300

    committing

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "https://keewee-be-652769a3d7b1.herokuapp.com",
+  // baseURL: "https://keewee-be-652769a3d7b1.herokuapp.com",
+  baseURL: "https://keewee-be-deploy-d4ae1bd2358e.herokuapp.com/api",
 });
 
 export default instance;
