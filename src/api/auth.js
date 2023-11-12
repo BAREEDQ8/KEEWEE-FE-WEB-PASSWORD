025d5e6bc6721584637a075c19b1f9f19e9965da
turki <trkyable@gmail.com>
2023-11-12 22:16:09 +0300

    committing

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import instance from ".";
 
 export const resetpassword = async (token, data) => {
   console.log(token, data);
-  const res = await instance.patch(`/api/auth/resetPassword/${token}`, data);
+  const res = await instance.patch(`/auth/resetPassword/${token}`, data);
   return res.data;
 };
 
