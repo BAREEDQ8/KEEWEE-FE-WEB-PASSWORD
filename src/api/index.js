68e3a38228c5360d72577b4e8b3513c8ee7209a8
turki <trkyable@gmail.com>
2023-12-17 18:09:00 +0300

    adding logo and validations

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
   // baseURL: "https://keewee-be-652769a3d7b1.herokuapp.com",
 
   // new one
-  // baseURL: "https://keewee-be-deploy-d4ae1bd2358e.herokuapp.com/api",
+  baseURL: "https://keewee-be-deploy-d4ae1bd2358e.herokuapp.com/api",
 });
 
 export default instance;
